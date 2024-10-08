aa29577ae7f2b0115001bc30819a12c36176ca52
essillore <riikka.pehkonen@me.com>
2024-10-08 11:19:06 +0300

    Hello

---
+++ Assets/MinunTestiKoodi.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinunTestiKoodi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+              if (Input.GetKey(KeyCode.Space))
+        {
+                transform.Translate(Vector3.up * Time.deltaTime * 5f, Space.World);
+            print("liikutus");
+        }
+    }
+
+}
